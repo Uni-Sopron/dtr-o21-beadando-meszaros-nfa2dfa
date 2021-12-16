42f3bda81928303fe4686c29c76f6451fb933b61
Mészáros Ádám <meszike91@gmail.com>
2021-12-17 00:12:16 +0100

    gitingore file added

---
+++ main.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import javax.sound.sampled.SourceDataLine;
+
+public class main {
+
+    public static void main(String[] args){
+        
+        NFA nfa = JsonToNFA();
+
+        nfa.dfaConverting();
+        
+    }
+    
+    public static NFA JsonToNFA(){
+
+        ObjectMapper objectMapper= new ObjectMapper();
+
+        NFA nfa=new NFA();
+    try{
+        nfa = objectMapper.readValue(new File("./nfa.json"), NFA.class);
+        } catch (JsonProcessingException e){
+            System.out.println("JsonProcessingException");
+            System.out.println(e.getMessage());
+        } catch (IOException io){
+            System.out.println("fájlolvalási hiba");
+        } 
+        return nfa;        
+}              
+}
